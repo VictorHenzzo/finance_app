bc11e1238829f7e2013e54f9306a13b255bebd04
vctr <victorhenzzowp@gmail.com>
2026-02-04 22:53:22 -0300

    feat: add crud user endpoints

--- src/main/java/com/wykmmm/financeApp/controllers/UserController.java
+++ src/main/java/com/wykmmm/financeApp/controllers/UserController.java
@@ -1,25 +1,47 @@
 package com.wykmmm.financeApp.controllers;
 
 import com.wykmmm.financeApp.data.dto.UserDto;
-import com.wykmmm.financeApp.models.UserModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("/me/{id}")
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
     public UserDto myUser(@PathVariable UUID id){
         UserDto user = new UserDto();
         user.setId(id);
         user.setName("Human name");
         user.setEmail("human@mail.com");
         return user;
+    }
+
+    @PostMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public UserDto register(@RequestBody UserDto user){
+        UserDto registeredUser = new UserDto();
+        registeredUser.setEmail(user.getEmail());
+        registeredUser.setName(user.getName());
+        registeredUser.setId(UUID.randomUUID());
+        return registeredUser;
+    }
+
+    @PutMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public UserDto update(@RequestBody UserDto user){
+        return user;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        return ResponseEntity.noContent().build();
     }
 }
